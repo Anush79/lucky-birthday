16d39376b0ac3b1450ef80ce9c681e420d3f63d7
Anush79 <66175237+Anush79@users.noreply.github.com>
2022-09-03 12:41:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lucky-birthday
+ This is just for fun website , There is no luck associated with our birthday. We believe in making our own luck. The website never shares your personal data to any other party as we do not store it anywhere , so just relax and play it with your friends.
---
+++ script.js
@@ -0,0 +1,42 @@
+var birthday = document.querySelector("#bday");
+var luckynum = document.querySelector("#lucky");
+var btn = document.querySelector("button");
+var output = document.querySelector(".result");
+var sum = 0;
+
+output.style="display:none;"
+birthday.onchange = function (){
+    output.style="display:block;"
+    sum = 0; return sum;};//refreshes system everytime bday gets changed
+
+//function to remove unwanted "-" from date
+function clipString(stringtokeep, unwanted) {
+    const clippedArray = stringtokeep.split(unwanted);
+    const NewString = clippedArray.join("");
+    return NewString;
+}
+
+//function to add digits of bday
+function findsum(x) {
+    for (let i = 0; i < x.length; i++) {
+        sum = sum + Number(x.charAt(i));
+    }
+    return sum;
+}
+
+//when user clicks button
+btn.addEventListener("click", function luckfinder() {
+    
+    
+    const NewDob = clipString(birthday.value, '-');
+    sum = findsum(NewDob);
+
+    
+
+    if (sum % luckynum.value == 0) {
+        output.innerText= "Yuhooooo!!! Your birthday is Luckyyyy 🎉🍀💯😎🤘"
+    }
+    else output.innerText="Aww ! It's okay , your birthday is not very lucky🙂";
+
+
+})
